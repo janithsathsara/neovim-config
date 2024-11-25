1a635f05c9bf7c6675d076a3551344d16ae7da4e
Janith Sathsara Herath <53212744+janithsathsara@users.noreply.github.com>
2024-11-25 10:21:30 +0530

    Added back remote flash functionality in flash.nvim
    Also added comments to explain the workflow

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -5,9 +5,9 @@ return {
     opts = {},
     -- stylua: ignore
     keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },            -- jump anywhere
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },            -- jump anywhere. Can be done with y or d as well.
         { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" }, -- jump and highlight treesitter nodes
-        -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },     -- yr{matching string}y will copy that line and jump back to the position you were in
         -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
         -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
