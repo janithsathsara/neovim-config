8c780e31e6182cb160eb79ffdffac54595f68073
Janith Sathsara Herath <53212744+janithsathsara@users.noreply.github.com>
2025-01-19 11:23:30 +0530

    Cleaned the mini-surround file

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -3,16 +3,9 @@ return {
     version = '*',
     config = function()
         require("mini.surround").setup(
-        -- No need to copy this inside `setup()`. Will be used automatically.
             {
-                -- Add custom surroundings to be used on top of builtin ones. For more
-                -- information with examples, see `:h MiniSurround.config`.
                 custom_surroundings = nil,
-
-                -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
                 highlight_duration = 500,
-
-                -- Module mappings. Use `''` (empty string) to disable one.
                 mappings = {
                     add = 'xa',            -- Add surrounding in Normal and Visual modes
                     delete = 'xd',         -- Delete surrounding
@@ -26,23 +19,9 @@ return {
                     suffix_next = 'n',     -- Suffix to search with "next" method
                 },
 
-                -- Number of lines within which surrounding is searched
                 n_lines = 20,
-
-                -- Whether to respect selection type:
-                -- - Place surroundings on separate lines in linewise mode.
-                -- - Place surroundings on each line in blockwise mode.
                 respect_selection_type = false,
-
-                -- How to search for surrounding (first inside current line, then inside
-                -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
-                -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
-                -- see `:h MiniSurround.config`.
                 search_method = 'cover',
-
-                -- Whether to disable showing non-error feedback
-                -- This also affects (purely informational) helper messages shown after
-                -- idle time if user input is required.
                 silent = false,
             }
         )
