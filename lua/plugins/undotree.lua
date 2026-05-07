2fb3840f8b8eafeb8adaa6e6d06c72b34fbef60a
janith <sathsarah@gmail.com>
2026-05-07 20:49:13 +0530

    removed undotree config as it is now core

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,9 +1,9 @@
-return {
-	"jiaoshijie/undotree",
-	lazy = true,
-	dependencies = "nvim-lua/plenary.nvim",
-	config = true,
-	keys = { -- load the plugin only when using it's keybinding:
-		{ "<leader>t", "<cmd>lua require('undotree').toggle()<cr>", desc = "undotree" },
-	},
-}
+return {}
+-- 	"jiaoshijie/undotree",
+-- 	lazy = true,
+-- 	dependencies = "nvim-lua/plenary.nvim",
+-- 	config = true,
+-- 	keys = { -- load the plugin only when using it's keybinding:
+-- 		{ "<leader>t", "<cmd>lua require('undotree').toggle()<cr>", desc = "undotree" },
+-- 	},
+-- }
