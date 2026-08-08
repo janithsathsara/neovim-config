7c20b26cb87611106d6b14abde2e60e4253868fb
janith <sathsarah@gmail.com>
2026-08-08 20:51:49 +0530

    Updated the fzf-lua config

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,23 +1,16 @@
 return {
 	{
 		"ibhagwan/fzf-lua",
-		-- optional for icon support
-		-- lazy = true,
+		lazy = true,
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		cmd = "FzfLua",
-		-- or if using mini.icons/mini.nvim
-		-- dependencies = { "nvim-mini/mini.icons" },
-		---@module "fzf-lua"
-		---@type fzf-lua.Config|{}
-		---@diagnostic disable: missing-fields
-		---@diagnostic enable: missing-fields
 		keys = {
-			{ "<leader>ff", "<cmd>FzfLua files<CR>", desc = "find files" },
-			{ "<leader>fw", "<cmd>FzfLua grep<CR>", desc = "find files" },
-			{ "<leader>sw", "<cmd>FzfLua grep_cword<CR>", desc = "grep word under cursor" },
-			{ "<leader>fr", "<cmd>FzfLua history<CR>", desc = "recent files" },
-			{ "<leader>fk", "<cmd>FzfLua keymaps<CR>", desc = "recent files" },
-			{ "<leader>fh", "<cmd>FzfLua helptags<CR>", desc = "recent files" },
+			{ "<leader>ff", "<cmd>FzfLua files<CR>", "n", desc = "find files" },
+			{ "<leader>fw", "<cmd>FzfLua grep<CR>", "n", desc = "find files" },
+			{ "<leader>sw", "<cmd>FzfLua grep_cword<CR>", "n", desc = "grep word under cursor" },
+			{ "<leader>fr", "<cmd>FzfLua history<CR>", "n", desc = "recent files" },
+			{ "<leader>fk", "<cmd>FzfLua keymaps<CR>", "n", desc = "recent files" },
+			{ "<leader>fh", "<cmd>FzfLua helptags<CR>", "n", desc = "recent files" },
 		},
 	},
 }
